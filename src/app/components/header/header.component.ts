67f784a29eb64913c74fec5a8ffd9d728c308eca
semihomur <semihomur23@gmail.com>
2019-12-05 19:21:31 +0300

    Added-darkmodeoption

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,11 +8,36 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  theme = true;
   constructor(private authService: AuthService,
               private alertify: AlertifyService) { }
 
   ngOnInit() {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'default-theme' || theme === null ) {
+      this.DefaultTheme();
+      this.theme = true;
+    } else {
+      this.DarkTheme();
+      this.theme = false;
+    }
+  }
+  DefaultTheme() {
+    document.body.classList.add('default-theme');
+    document.body.classList.remove('dark-theme');
+  }
+  DarkTheme() {
+    document.body.classList.add('dark-theme');
+    document.body.classList.remove('default-theme');
+  }
+  changeTheme() {
+    this.theme = !this.theme;
+    if (this.theme) {
+      this.DefaultTheme();
+    } else {
+      this.DarkTheme();
+    }
+    localStorage.setItem('theme', this.theme ? 'default-theme' : 'dark-theme');
   }
   loggedIn() {
     return this.authService.loggedIn();
